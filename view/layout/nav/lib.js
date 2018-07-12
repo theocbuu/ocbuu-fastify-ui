055e11884b2b62b4394785da33528a83d9e26f27
Giang Phan <giangphan@GP-EAMac.local>
2018-07-12 16:52:08 -0400

    working on nav active

--- view/layout/nav/lib.js
+++ view/layout/nav/lib.js
@@ -1,15 +1,19 @@
 
 // Get the container element
-var btnContainer = document.getElementById("myDIV");
+//var btnContainer = document.getElementById("myDIV");
 
 // Get all buttons with class="btn" inside the container
-var btns = btnContainer.getElementsByClassName("btn");
+//var btns = btnContainer.getElementsByClassName("btn");
 
 // Loop through the buttons and add the active class to the current/clicked button
-for (var i = 0; i < btns.length; i++) {
-    btns[i].addEventListener("click", function() {
-        var current = document.getElementsByClassName("active");
-        current[0].className = current[0].className.replace(" active", "");
-        this.className += " active";
-    });
-}        
+// for (var i = 0; i < btns.length; i++) {
+//     btns[i].addEventListener("click", function() {
+//         var current = document.getElementsByClassName("active");
+//         current[0].className = current[0].className.replace(" active", "");
+//         this.className += " active";
+//     });
+// }        
+
+$(document).ready(function(){
+    document.getElementById("testing").innerHTML = "<a class='nav-link' href='#'>test</a>"
+});
